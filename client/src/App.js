5e4d370e4e7d78609ece724a1d426fca60cd6b99
arrafi311-stack <arrafi311@gmail.com>
2025-11-02 16:09:34 +0900

    Change socket connection URL to production

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://taboogame-14.onrender.com/");
 
 // Example taboo words for each card
 const TABOO_WORDS = {
